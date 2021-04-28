cfc123dae97be0c2ee6c2bb77bfbf4beb5fba375
전영호 <77596160+Hprogram@users.noreply.github.com>
2021-04-28 17:37:07 +0900

    Delete menudao-client directory
    PR 에러로 인한 삭제.

--- menudao-client/src/App.js
+++
@@ -1,9 +0,0 @@
-//import "./App.css";
-import Main from "./pages/Main";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-function App() {
-  return <Main />;
-}
-
-export default App;
