9f06b147940e4a045fd195049d2d877f31e998c1
Jeong-HW <gusdnd114@naver.com>
2021-05-02 20:30:52 +0900

    add client_React Router

--- menudao-client/src/App.js
+++ menudao-client/src/App.js
@@ -1,27 +1,59 @@
 import "./App.css";
 import Main from "./pages/Main";
 import Select from "./pages/Select";
-import Mypage from "./pages/Mypage";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+// 로그인 성공 시, 서버에서 받은 "accessToken"값을 가져와서 조건부로 렌더링한다
 function App() {
   return (
     <Router>
-      <Nav />
       <Switch>
         <Route exact path="/">
           <Main />
         </Route>
+        {/* <Nav /> */}
         <Route path="/select">
           <Select />
         </Route>
-        {/* <Route exact path="/mypage">
-          <Mypage />
-        </Route> */}
       </Switch>
     </Router>
   );
+
+  // return (
+  //   <div>
+  //     <Switch>
+  //       <Route
+  //         path="/login"
+  //         render={() => (
+  //           <Login
+  //             handleResponseSuccess={this.handleResponseSuccess.bind(this)}
+  //           />
+  //         )}
+  //       />
+  //       <Route exact path="/signup" render={() => <Signup />} />
+  //       <Route
+  //         exact
+  //         path="/mypage"
+  //         render={() => (
+  //           <Mypage
+  //             userinfo={userinfo}
+  //             handleLogout={this.handleLogout.bind(this)}
+  //           />
+  //         )}
+  //       />
+  //       <Route
+  //         path="/"
+  //         render={() => {
+  //           if (isLogin) {
+  //             return <Redirect to="/mypage" />;
+  //           }
+  //           return <Redirect to="/login" />;
+  //         }}
+  //       />
+  //     </Switch>
+  //   </div>
+  // );
 }
 
 export default App;
