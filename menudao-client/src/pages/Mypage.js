4f8ecfc4e836bcdd186c4268b261d1ff22b05a3f
Jeong-HW <gusdnd114@naver.com>
2021-05-03 21:10:20 +0900

    add client_setting basic structure

--- menudao-client/src/pages/Mypage.js
+++ menudao-client/src/pages/Mypage.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Nav from "../components/Nav";
+import Userinfo from "../components/Userinfo";
 import { useSelector, useDispatch } from "react-redux";
+// Userinfo 컴포넌트를 불러와서 렌더링한다
 
 function Mypage() {
   return (
     <>
-      <h1>마이 페이지임</h1>
+      <Nav />
+      <Userinfo />
     </>
   );
 }
