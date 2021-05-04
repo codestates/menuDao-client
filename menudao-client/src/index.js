927b4d2e515b0f7e3c2c2590fa524a0fe5464ac4
Jeong-HW <gusdnd114@naver.com>
2021-05-04 23:40:04 +0900

    add client_CookiesProvider

--- menudao-client/src/index.js
+++ menudao-client/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import store from "./store/store";
 import { Provider } from "react-redux";
+import { CookiesProvider } from "react-cookie";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <CookiesProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </CookiesProvider>,
   document.getElementById("root")
 );
