67b13ad6fd4c22bcfc7c616d23e01e413f5679ac
allofhyuk <gggg21367@gmail.com>
2021-05-04 11:34:50 +0900

    add client_mypage_CSS & js

--- menudao-client/src/components/Userinfo.js
+++ menudao-client/src/components/Userinfo.js
@@ -1,9 +1,22 @@
 import React from "react";
+import "../css/mypage.css";
 
 function Userinfo() {
   return (
     <>
-      <h1>하윙 나 마이페이지</h1>
+      <div id="#mypageLogo-container">
+        <div>마이페이지</div>
+      </div>
+      <div id="#mypageContents-container">
+        <div>
+          <div>
+            <div id="#mypage-id">ID</div>
+          </div>
+          <div id="#mypage-name">이름</div>
+          <div id="#mypage-sex">성별</div>
+        </div>
+        <button>Edit</button>
+      </div>
     </>
   );
 }
