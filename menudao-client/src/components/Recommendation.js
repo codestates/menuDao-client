92dec5e0e787f305e1862a493794f26f7cedcaf2
allofhyuk <gggg21367@gmail.com>
2021-05-05 02:25:00 +0900

    add client_recommendation_components&CSS style

--- menudao-client/src/components/Recommendation.js
+++ menudao-client/src/components/Recommendation.js
@@ -86,19 +86,21 @@ function Recommendation() {
     <>
       <div id="common-container">
         {/* menu-choice에서 food-name이랑 카테고리도 서버에서 받아 와야할 것 같다! */}
-        <div className="food-icon-container">
+        <div id="food-icon-container">
           {/* 아이콘은 public에 있슴다, 경로는 Nav.js 로고 가져온 거 확인 해주세요! */}
           {/* 서버에서 받아온 카테고리에 따라 다른 아이콘이 뜨도록 조건문 필요할 것 같슴다! */}
           <img className="food-category-icon"></img>
           {/* span font-size 크게! */}
-          <p className="food-name"></p>
+          <div className="food-name">{Recommend_Food}"마카롱"</div>
         </div>
         <div id="recommend-container">
-          <p className="recommend-subtitle">오늘은 {Recommend_Food} 어때?</p>
-          <button className="positive-btn">이 메뉴로 할래요</button>
-          <button className="negative-btn" onClick={() => getAnotherFood()}>
-            다른 메뉴는요?
-          </button>
+          <p className="recommend-subtitle">오늘은 {Recommend_Food}마카롱 어떤가요?</p>
+          <div className="rec-btn-container">
+            <button className="positive-btn">이 메뉴로 할래요</button>
+            <button className="negative-btn" onClick={() => getAnotherFood()}>
+              다른 메뉴는요?
+            </button>
+          </div>
         </div>
       </div>
     </>
