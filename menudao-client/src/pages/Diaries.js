c08b92e70a1c1df65c1c86a2b22421be654df4ed
Jeong-HW <gusdnd114@naver.com>
2021-05-04 23:40:42 +0900

    add client_get DiaryList Data

--- menudao-client/src/pages/Diaries.js
+++ menudao-client/src/pages/Diaries.js
@@ -1,13 +1,36 @@
 // 다이어리들이 나오는 목록 페이지
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import DiaryCapsule from "../components/DiaryCapsule";
 import Nav from "../components/Nav";
+import swal from "sweetalert";
 
 // Diary.js를 사용하여 저장된 목록들만큼 map 함수를 사용하여 랜더링한다
 function Diarylist() {
+  const [Diarylist, setDiarylist] = useState([]);
+
+  const handleGetDiarylist = () => {
+    axios
+      .post("http://localhost:4000/diary-list", {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+      })
+      .then((res) => {
+        setDiarylist(res.data.diaries);
+      })
+      .catch((err) => {
+        console.log(err);
+        swal("데이터를 읽어오지 못하였습니다", "", "error");
+      });
+  };
+  useEffect(() => handleGetDiarylist(), []);
+
   return (
     <>
       <Nav />
-      <h1>여긴 다이어리 목록들이 나옴</h1>
+      {Diarylist.map((Diary) => (
+        <DiaryCapsule />
+      ))}
     </>
   );
 }
