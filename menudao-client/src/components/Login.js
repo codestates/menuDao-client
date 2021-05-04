b00cdb9f3e605b7f0ea7d7aec7b6ece088793fb4
Jeong-HW <gusdnd114@naver.com>
2021-05-04 19:40:49 +0900

    fix client code data->cookie

--- menudao-client/src/components/Login.js
+++ menudao-client/src/components/Login.js
@@ -25,8 +25,8 @@ function Login({ turnOffModal_CSS, setLoginModal, setJoinModal }) {
         }
       )
       .then((res) => {
-        console.log("accessToken: ", res.data.accessToken);
-        dispatch(userLogin(res.data.accessToken));
+        console.log("accessToken: ", res.cookie.accessToken);
+        dispatch(userLogin(res.cookie.accessToken));
         swal("로그인되었습니다", "", "success");
         history.push("/select");
       })
