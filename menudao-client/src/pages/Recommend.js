cc66d7a335882dba6ee77e41ebc0bb740e959162
Jeong-HW <gusdnd114@naver.com>
2021-05-04 19:47:20 +0900

    add client_ comment

--- menudao-client/src/pages/Recommend.js
+++ menudao-client/src/pages/Recommend.js
@@ -3,7 +3,6 @@ import React from "react";
 import Nav from "../components/Nav";
 import Recommendation from "../components/Recommendation";
 
-//Recommendation 에서 initialState에 저장한 food name을 useSelector 하여 사용한다
 function Recommend() {
   return (
     <>
