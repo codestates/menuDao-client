77a891732c1236879fd364ba6773ce0b87453a84
Jeong-HW <gusdnd114@naver.com>
2021-05-04 22:14:44 +0900

    delete client_LoginReducer

--- menudao-client/src/module/index.js
+++ menudao-client/src/module/index.js
@@ -1,10 +1,9 @@
 import { combineReducers } from "redux";
-import LoginReducer from "./loginModal";
+
 import validationReducer from "./validationCheck";
 import foodInfoReducer from "./RecommendFood";
 import weatherInfoReducer from "./WeatherInfo";
 const rootReducer = combineReducers({
-  LoginReducer,
   validationReducer,
   foodInfoReducer,
   weatherInfoReducer,
