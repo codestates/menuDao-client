ac5c885ee1d7a70dcb9d0dcb702cb932ced98b4e
Jeong-HW <gusdnd114@naver.com>
2021-05-04 19:41:20 +0900

    add client_ Logout function

--- menudao-client/src/components/Nav.js
+++ menudao-client/src/components/Nav.js
@@ -1,14 +1,33 @@
 // 왼쪽 상단에는 로고(클릭 시 다시 처음부터 선택하는 화면) , 오른쪽 상단에는 마이페이지랑 로그아웃 버튼
-// 날씨, 기분, 음식 대분류 선택하는 페이지
+import axios from "axios";
 import React from "react";
 import { useHistory } from "react-router";
+import { useDispatch } from "react-redux";
+import { userLogin } from "../module/loginModal";
+import swal from "sweetalert";
 import "../css/nav.css";
 
 //  마이페이지, 다이어리, 로그아웃 버튼 생성 (axios 요청 총 3개 구현)
-//  initialState에 저장된 accessToken을 가지고 post요청을 보낸다
 //  버튼 클릭하면 각각의 페이지로 이동 (Redirect)
 function Nav() {
+  const dispatch = useDispatch();
   const history = useHistory();
+
+  // 로그아웃 함수
+  const handlelogOut = () => {
+    axios
+      .post("http://localhost:4000/signout")
+      .then(() => {
+        dispatch(userLogin(""));
+        swal("로그아웃 되었습니다", "", "success");
+        history.push("/main");
+      })
+      .catch((err) => {
+        console.log(err);
+        swal("로그아웃 실패");
+      });
+  };
+
   return (
     <>
       <div id="nav-container">
@@ -38,7 +57,9 @@ function Nav() {
           >
             Diary
           </button>
-          <button id="logout_btn">Log out</button>
+          <button id="logout_btn" onClick={() => handlelogOut()}>
+            Log out
+          </button>
         </div>
       </div>
     </>
