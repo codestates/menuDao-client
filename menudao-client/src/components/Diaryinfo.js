615b9e7a1439ea067a85b2fb3018e30c2f02ce90
allofhyuk <gggg21367@gmail.com>
2021-05-05 23:42:48 +0900

    fix client_Diary_layout&&hook

--- menudao-client/src/components/Diaryinfo.js
+++ menudao-client/src/components/Diaryinfo.js
@@ -1,27 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import "../css/diary.css";
 
-function Diaryinfo({ }) {
+function Diaryinfo() {
+  const [fileUrl, setFileUrl] = useState(null);
+  const [isClick, setIsClick] = useState(false);
+  const [comment, setComment] = useState('');
+  console.log(comment);
+  function processImage(event){
+    const imageFile = event.target.files[0];
+    if(imageFile) {
+      const imageUrl = URL.createObjectURL(imageFile);
+      setFileUrl(imageUrl)
+    }else {
+      setFileUrl(null);
+    }
+  }
+  
   return (
     <>
       <div id="side-bar">
-        <div className="side-title">MY PAGE</div>
+        <div className="side-title">Diary</div>
       </div>
-      <div id="mypageContents-container">
-        <button id="mypage-edit-btn" onClick={() => {setIsClick(true)}}><i className="fas fa-pencil-alt"></i></button>
-        <div className="mypage-title">USER INFOMATION</div>
-        <div id="mypage-main-container">
-          <div id="userinfo-subtitle">
-            <div className="mypage-id">ID</div>
-            <div className="mypage-name">이름</div>
-            <div className="mypage-birth">생년월일</div>
-            <div className="mypage-sex">성별</div>
+      <div id="diaryinfo-container">
+        <div className="diaryinfo-title">MY DAIRY</div>
+        <div id="diaryinfo-upper-container">
+          <div id="diaryinfo-subtitle">
+            <span className="diaryinfo-food-name">돼지 국밥</span>
+            <span className="daryinfo-date">2021.05.05</span>
+            {/* 기분: 아이콘, 음식대분류: 이미지 */}
+            
+              <i className="fas fa-grin-hearts"></i>
+              <img className="food-icon" src="./food_icon/bibimbap.png"></img>
+              <img className="weather-icon" src="./weather_icon/sun.png"></img>    
           </div>
-          <div id="userinfo-container">
-            <div className="user-id">{MypageInfo.user_id}</div>
-            <div className="user-name">{MypageInfo.user_name}</div>
-            <div className="user-birth">{MypageInfo.user_birthday}</div>
-            <div className="user-sex">{MypageInfo.user_sex}</div>
+          <div id="diary-img-container">
+          {fileUrl 
+          ? <img className="upload-img" src={fileUrl}></img>
+          : <div className="upload-img-thum"></div>
+          }
+          <label className="upload-btn" htmlFor="input-file">PHOTO UPLOAD</label>
+          <input type="file" id="input-file" accept="image/*" onChange={processImage}></input>
           </div>
+        </div>
+        <div id="diaryinfo-lower-container">
+        <div className="comment-container">
+          <div className="diaryinfo-title">COMMENT</div>
+            { !isClick 
+              ? <button id="diary-edit-btn" onClick={() => {setIsClick(true)}}><i id="feel-good" className="fas fa-pencil-alt"></i></button>
+              : <button id="diary-save-btn" onClick={() => {setIsClick(false)}}>SAVE</button>
+            }
+        </div>
+          {!isClick
+            ? <div className="diaryinfo-comment">{comment}</div>
+            : <input className="diaryinfo-comment-input" value={comment} onChange={(e) => {setComment(e.target.value)}}></input>
+          }
         </div>
       </div>
     </>
--- menudao-client/src/pages/Diary.js
+++ menudao-client/src/pages/Diary.js
@@ -1,14 +1,18 @@
 // 다이어리 목록에서 클릭 했을 때 나오는 상세 페이지
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Diaryinfo from "../components/Diaryinfo"
+import Nav from "../components/Nav"
 import axios from "axios";
 
 // GET: /diary (다이어리 목록 중 하나 클릭 시 하나의 다이어리 정보를 받음)
 // DELETE: /diary-list (다이어리 목록 중 클릭한 것들을 삭제하고 id값들을 배열형태로 넘겨준다)
 // comment 필요
 function Diary() {
+
   return (
     <>
-      <div> 다이어리 목록 클릭하면 나오는 개별 페이지</div>
+      <Nav />
+      <Diaryinfo />
     </>
   );
 }
