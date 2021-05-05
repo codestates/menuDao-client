474ac67212c88d7790f6f88eeab3d1d404bc48f4
Jeong-HW <gusdnd114@naver.com>
2021-05-05 14:51:42 +0900

    add client_basic structure

--- menudao-client/src/components/DiaryCapsule.js
+++ menudao-client/src/components/DiaryCapsule.js
@@ -1,11 +1,15 @@
 // 다이어리들이 나오는 목록 페이지
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-// Diary.js를 사용하여 저장된 목록들만큼 map 함수를 사용하여 랜더링한다
-function DiaryCapsule() {
+// GET: /diary (다이어리 목록 중 하나 클릭 시 하나의 다이어리 정보를 받음)
+// DELETE: /diary-list (다이어리 목록 중 클릭한 것들을 삭제하고 id값들을 배열형태로 넘겨준다)
+// comment 필요
+function DiaryCapsule({ key, feeling, weather, big_choice_menu, choice_menu }) {
+  const [isClick, setisClick] = useState("false");
   return (
     <>
-      <h1>여긴 다이어리 목록들이 나옴</h1>
+      <div className="diary"></div>
     </>
   );
 }
