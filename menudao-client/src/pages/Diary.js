cfe02aebb939dcf92fd14094a5463f5f8efd5f55
HPROGRAM <wjs2096@gmail.com>
2021-05-06 05:08:36 +0900

    update Client_pages

--- menudao-client/src/pages/Diary.js
+++ menudao-client/src/pages/Diary.js
@@ -1,12 +1,13 @@
 // 다이어리 목록에서 클릭 했을 때 나오는 상세 페이지
 import React, { useState, useEffect } from "react";
-import Diaryinfo from "../components/Diaryinfo"
-import Nav from "../components/Nav"
+import Diaryinfo from "../components/Diaryinfo";
+import Nav from "../components/Nav";
 import swal from "sweetalert";
 import axios from "axios";
 import { useLocation } from "react-router";
 import dotenv from "dotenv";
 dotenv.config();
+axios.defaults.withCredentials = true;
 
 // GET: /diary (다이어리 목록 중 하나 클릭 시 하나의 다이어리 정보를 받음)
 // DELETE: /diary-list (다이어리 목록 중 클릭한 것들을 삭제하고 id값들을 배열형태로 넘겨준다)
@@ -18,19 +19,17 @@ function Diary() {
   const [DiaryInfo, setDiaryInfo] = useState({});
 
   const handleGetDiaryInfo = function () {
+    console.log("choiceID:", choiceID);
     axios
       .get(
-        `${process.env.REACT_APP_HTTP}://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/diary`,
-        {
-          id: choiceID,
-        },
+        `${process.env.REACT_APP_HTTP}://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/diary?id=${choiceID}`,
         {
           headers: { "Content-Type": "application/json" },
           withCredentials: true,
         }
       )
       .then((res) => {
-        setDiaryInfo(res.data.diarydata);
+        setDiaryInfo({ ...res.data.diarydata });
         console.log("정보전송 성공");
       })
       .catch((err) => {
@@ -46,6 +45,7 @@ function Diary() {
       <Nav />
       <Diaryinfo
         key={DiaryInfo.id}
+        id={DiaryInfo.id}
         feeling={DiaryInfo.feeling}
         weather={DiaryInfo.weather}
         big_choice_menu={DiaryInfo.big_choice_menu}
