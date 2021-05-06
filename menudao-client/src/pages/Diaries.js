07c6c7a0927f8724b9c6d2dd51eab5fc3084b33d
allofhyuk <gggg21367@gmail.com>
2021-05-06 22:20:40 +0900

    client_Diaries layout&style

--- menudao-client/src/pages/Diaries.js
+++ menudao-client/src/pages/Diaries.js
@@ -11,44 +11,7 @@ axios.defaults.withCredentials = true;
 
 // Diary.js를 사용하여 저장된 목록들만큼 map 함수를 사용하여 랜더링한다
 function Diarylist() {
-  const [Diarylist, setDiarylist] = useState([
-    {
-      id: 1,
-      feeling: "좋음",
-      weather: "눈",
-      big_choice_menu: "한식",
-      choice_menu: "볶음밥",
-      date: "2020/05/05",
-      comment: "오늘은 보끔밥을 먹었따",
-    },
-    {
-      id: 2,
-      feeling: "나쁨",
-      weather: "흐림",
-      big_choice_menu: "중식",
-      choice_menu: "짬뽕",
-      date: "2020/05/05",
-      comment: "날씨가 구리구리해서 국물이 땡김...",
-    },
-    {
-      id: 3,
-      feeling: "조금나쁨",
-      weather: "맑음",
-      big_choice_menu: "디저트",
-      choice_menu: "마카롱",
-      date: "2020/05/05",
-      comment: "막하롱 맛있지...",
-    },
-    {
-      id: 4,
-      feeling: "조금나쁨",
-      weather: "맑음",
-      big_choice_menu: "디저트",
-      choice_menu: "마카롱",
-      date: "2020/05/05",
-      comment: "ㅋㅋ",
-    },
-  ]);
+  const [Diarylist, setDiarylist] = useState([]);
 
   const handleGetDiarylist = function () {
     axios
@@ -70,52 +33,6 @@ function Diarylist() {
   };
   useEffect(() => handleGetDiarylist(), []);
 
-  const Category_IconClassName = function () {
-    if (Diarylist.big_choice_menu === "한식") {
-      setbigchoiceicon("./food_icon/bibimbap.png");
-    } else if (Diarylist.big_choice_menu === "일식") {
-      setbigchoiceicon("./food_icon/nigiri.png");
-    } else if (Diarylist.big_choice_menu === "중식") {
-      setbigchoiceicon("./food_icon/chinese.png");
-    } else if (Diarylist.big_choice_menu === "양식") {
-      setbigchoiceicon("./food_icon/steak.png");
-    } else if (Diarylist.big_choice_menu === "분식&패스트푸드") {
-      setbigchoiceicon("./food_icon/fishcake.png");
-    } else if (Diarylist.big_choice_menu === "야식&안주") {
-      setbigchoiceicon("./food_icon/soju.png");
-    } else if (Diarylist.big_choice_menu === "디저트") {
-      setbigchoiceicon("./food_icon/macaron.png");
-    }
-  };
-
-  const Weather_IconClassName = function () {
-    if (Diarylist.weather === "눈") {
-      setweatherIcon("./weather_icon/snowman.png");
-    } else if (Diarylist.weather === "비") {
-      setweatherIcon("./weather_icon/raining.png");
-    } else if (Diarylist.weather === "흐림") {
-      setweatherIcon("./weather_icon/clouds.png");
-    } else if (Diarylist.weather === "맑음") {
-      setweatherIcon("./weather_icon/sun.png");
-    }
-  };
-
-  const Feeling_IconClassName = function () {
-    if (Diarylist.feeling === "나쁨") {
-      setfeelingIcon("fas fa-grimace");
-    } else if (Diarylist.feeling === "조금나쁨") {
-      setfeelingIcon("fas fa-frown");
-    } else if (Diarylist.feeling === "평범") {
-      setfeelingIcon("fas fa-smile");
-    } else if (Diarylist.feeling === "좋음") {
-      setfeelingIcon("fas fa-grin-hearts");
-    }
-  };
-
-  useEffect(() => Category_IconClassName(), []);
-  useEffect(() => Weather_IconClassName(), []);
-  useEffect(() => Feeling_IconClassName(), []);
-
   return (
     <>
       <Nav />
@@ -124,15 +41,18 @@ function Diarylist() {
       </div>
       <div id="diary-list-container">
         {Diarylist.length === 0 ? (
-          <div>다이어리 목록이 비어있습니다</div>
+          <div className="diaries-empty">
+            <img alt="empty" src="./folder.png" />
+            <div className="diaries-empty-inner">다이어리 목록이 비어있습니다</div>
+          </div>
         ) : (
           Diarylist.map((Diary) => (
             <DiaryCapsule
               key={Diary.id}
               id={Diary.id}
-              feeling={feelingIcon}
-              weather={weatherIcon}
-              big_choice_menu={bigchoiceicon}
+              feeling={Diary.feeling}
+              weather={Diary.weather}
+              big_choice_menu={Diary.big_choice_menu}
               choice_menu={Diary.choice_menu}
               date={Diary.date}
               comment={Diary.comment}
